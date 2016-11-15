2cb3bc9481e2203bbd48135bab835a97e3b79c21
mayadi1 <admin@ayadios.com>
2016-11-15 00:48:23 -0800

    pod installed

---
+++ Pods/Target Support Files/Pods-Clash of Score/Pods-Clash of Score-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Clash_of_Score : NSObject
+@end
+@implementation PodsDummy_Pods_Clash_of_Score
+@end
---
+++ Pods/Target Support Files/Pods-Clash of Score/Pods-Clash of Score-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Clash_of_ScoreVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Clash_of_ScoreVersionString[];
+
