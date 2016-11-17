ce842fcf6c26b72cf137807b4c7f0b370b56237b
mayadi1 <admin@ayadios.com>
2016-11-16 19:21:33 -0800

    1 screen

--- Clash of Score/ServerViewController.swift
+++ Clash of Score/ServerViewController.swift
@@ -9,13 +9,37 @@
 import UIKit
 import Firebase
 import FirebaseDatabase
+import CoreMotion
+import AudioToolbox
 
 class ServerViewController: UIViewController {
     @IBOutlet weak var apple: UIImageView!
     @IBOutlet weak var target: UIImageView!
-    
+    var motionManager: CMMotionManager!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        motionManager = CMMotionManager()
+        //motionManager.accelerometerUpdateInterval = 0.1
+        
+        motionManager.startAccelerometerUpdates()
+        
+        _ = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ServerViewController.update), userInfo: nil, repeats: true)
+        
+        
+        newUser()
+        
+        //Vibrate
+        //self.vibrate()
+        
+//        let refScore = FIRDatabase.database().reference().child("users").child(UIDevice.current.identifierForVendor!.uuidString).child("Score")
+//        
+//        refScore.observe(FIRDataEventType.value, with: { (snapshot) in
+//            
+//            if snapshot.value != nil{
+//                self.scoreButton.setTitle(snapshot.value as! String?, for: .normal)}
+//        })
+
         
      let ref = FIRDatabase.database().reference()
         
@@ -82,4 +106,42 @@ class ServerViewController: UIViewController {
         
         
     }
+    func update () {
+        if (motionManager.isAccelerometerActive) {
+            
+            //Firebase fatabase ref
+            let ref = FIRDatabase.database().reference()
+            
+            //create my current point
+            
+            if (motionManager.accelerometerData?.acceleration.x != nil && motionManager.accelerometerData?.acceleration.z != nil){
+            let myPoint = [motionManager.accelerometerData?.acceleration.x, motionManager.accelerometerData?.acceleration.z]
+            
+            //send my curretn point to firebase
+            ref.child("target").setValue(myPoint)
+            }
+        }
+    }
+    
+    //Vibrate
+    func vibrate(){
+        AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+        
+    }
+    
+    
+    
+    @IBAction func buckButtonPressed(_ sender: AnyObject) {
+        self.dismiss(animated: false) {
+            self.motionManager.stopAccelerometerUpdates()
+        }
+    }
+    
+    //new User Setup
+    func newUser(){
+        
+        let ref = FIRDatabase.database().reference()
+        ref.child("users").child(UIDevice.current.identifierForVendor!.uuidString).child("Score").setValue("0")
+    }
+
 }
